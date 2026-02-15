ec6ce3956542d65672d65ee60ba9847e5bac7f88
rajbansi141 <rajbansi141@gmail.com>
2026-02-15 20:39:44 +0545

    db

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express(); 
 const userRouter = require('./routes/user.routes');
@@ -15,6 +16,7 @@ const cookieParser = require('cookie-parser');
 const indexRouter = require('./routes/index.routes');
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'Views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(cookieParser());
